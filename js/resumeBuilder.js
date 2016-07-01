7d315a3693bf96790192d19a5ad37464f01e2a29
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-07-01 12:44:22 -0400

    feat: Add work object to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,32 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- 
- $("#main").append("Ruben Vazquez");
+var work = {
+	"jobs" : [
+	{
+		"employer" : "University of Central Florida",
+		"title" : "Peer Tutor",
+		"location" : "Orlando, FL",
+		"datesWorked" : "January 2015 - Present"
+		"description" : "- Attended workshops monthly to enhance acquired tutoring skills and learn new techniques.\n- Provided effective study skills for students in a series of weekly study sessions.\n- Established connections between abstract concepts and concrete real-world applications.\n- Networked with other students and tutors daily to build rapport and to gain insight on different tutoring techniques."
+	},
+	{
+		"employer" : "University of Central Florida",
+		"title" : "Undergraduate Teaching Assistant",
+		"location" : "Orlando, FL",
+		"datesWorked" : "January 2016 - May 2016",
+		"description" : "- Met with the course professor weekly to discuss upcoming course material and class agenda.\n- Assisted students with programming questions and concerns.\n- Provided rapid feedback on assignment submissions to clarify pitfalls and ambiguities.\n- Attended the course lecture to assist with the distribution of course material"
+	},
+	{
+		"employer" : "University of Central Florida",
+		"title" : "Supplemental Instruction Leader",
+		"location" : "Orlando, FL",
+		"datesWorked" : "January 2015 - June 2015",
+		"description" : "- Attended weekly meetings to discuss responsibilities and SI events.\n- Attended lecture twice a week to be kept up to date on course material.\n- Hosted four SI sessions per week to provide assistance with course material for students.\n- Met with the lecture professor weekly to gain input on student tendencies."
+	},
+	{
+		"employer" : "Palm Beach State College",
+		"title" : " OPS Professional",
+		"location" : "Lake Worth, FL",
+		"datesWorked" : "August 2013 - June 2014",
+		"description" : "- Obtained College Reading and Learning Association (CRLA) Level III tutor certification.\n- Scheduled appointments with students to receive help from tutors.\n- Served as an ambassador to inform the college community about tutoring benefits.\n- Responsible for upholding the goals and standards of the college and being a role model for students"
+	}
+	]
+}
