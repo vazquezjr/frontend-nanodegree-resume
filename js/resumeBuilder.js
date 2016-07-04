a5833e6499645200665c035ffda06380fc83efcb
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-07-04 15:45:50 -0400

    Changed minor details
    Renamed variables to match project format.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,40 +3,40 @@ var work = {
         "employer": "University of Central Florida",
         "title": "Peer Tutor",
         "location": "Orlando, FL",
-        "datesWorked": "January 2015 - Present",
+        "dates": "January 2015 - Present",
         "description": "- Attended workshops monthly to enhance acquired tutoring skills and learn new techniques.<br>- Provided effective study skills for students in a series of weekly study sessions.<br>- Established connections between abstract concepts and concrete real-world applications.<br>- Networked with other students and tutors daily to build rapport and to gain insight on different tutoring techniques."
     }, {
         "employer": "University of Central Florida",
         "title": "Undergraduate Teaching Assistant",
         "location": "Orlando, FL",
-        "datesWorked": "January 2016 - May 2016",
+        "dates": "January 2016 - May 2016",
         "description": "- Met with the course professor weekly to discuss upcoming course material and class agenda.<br>- Assisted students with programming questions and concerns.<br>- Provided rapid feedback on assignment submissions to clarify pitfalls and ambiguities.<br>- Attended the course lecture to assist with the distribution of course material"
     }, {
         "employer": "University of Central Florida",
         "title": "Supplemental Instruction Leader",
         "location": "Orlando, FL",
-        "datesWorked": "January 2015 - June 2015",
+        "dates": "January 2015 - June 2015",
         "description": "- Attended weekly meetings to discuss responsibilities and SI events.<br>- Attended lecture twice a week to be kept up to date on course material.<br>- Hosted four SI sessions per week to provide assistance with course material for students.<br>- Met with the lecture professor weekly to gain input on student tendencies."
     }, {
         "employer": "Palm Beach State College",
         "title": " OPS Professional",
         "location": "Lake Worth, FL",
-        "datesWorked": "August 2013 - June 2014",
+        "dates": "August 2013 - June 2014",
         "description": "- Obtained College Reading and Learning Association (CRLA) Level III tutor certification.<br>- Scheduled appointments with students to receive help from tutors.<br>- Served as an ambassador to inform the college community about tutoring benefits.<br>- Responsible for upholding the goals and standards of the college and being a role model for students"
     }]
 };
 
 var projects = {
-    "project": [{
+    "projects": [{
         "title": "College Event Website",
-        "datesWorked": "May 2016 - Present",
+        "dates": "May 2016 - Present",
         "description": "A website that allows users to view/create events pertaining to the college/university of the student(s).<br>Login and authentication enforced by Joomla to provide users with the proper ability to view and create events. Database created by MySQL Workbench to store and manage information regarding the access level of users, the events, and the participating universities. PHP and JavaScript used for providing users with a dynamic, on-the-spot updating, user experience.",
         "images": [
             "images/collegeevent.jpg"
         ]
     }, {
         "title": "Boggle",
-        "datesWorked": "May 2015 - August 2015",
+        "dates": "May 2015 - August 2015",
         "description": "An application that models the popular board game Boggle using Java.<br>The application features a graphical user interface which shows the board of sixteen dice, a panel that shows the words that user has found, a timer to keep track of the time remaining, and a panel that shows the current word being constructed among the various other features of the application.",
         "images": [
             "images/boggle.jpg"
@@ -68,7 +68,8 @@ var education = {
         "majors": [
             "Computer Engineering"
         ],
-        "datesAttended": "August 2014 - May 2017",
+        "dates": "August 2014 - May 2017",
+        "url": "",
     }, {
         "name": "Palm Beach State College",
         "location": "Lake Worth, FL",
@@ -76,7 +77,8 @@ var education = {
         "majors": [
             "Computer Engineering"
         ],
-        "datesAttended": "August 2010 - May 2014",
+        "dates": "August 2010 - May 2014",
+        "url": "",
     }],
     "onlineCourses": [{
         "title": "Front-End Web Developer",
@@ -110,7 +112,7 @@ work.display = function() {
         workEmployerString = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
         workTitleString = HTMLworkTitle.replace("%data%", work.jobs[i].title);
         workLocationString = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-        workDatesString = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+        workDatesString = HTMLworkDates.replace("%data%", work.jobs[i].dates);
         workDescriptionString = HTMLworkDescription.replace("%data%", work.jobs[i].description);
         $(".work-entry:last").append(workEmployerString + workTitleString);
         $(".work-entry:last").append(workLocationString);
@@ -122,18 +124,18 @@ work.display = function() {
 
 projects.display = function() {
 
-    for (var i = 0; i < projects.project.length; i++) {
+    for (var i = 0; i < projects.projects.length; i++) {
 
         $("#projects").append(HTMLprojectStart);
-        var projectTitleString = HTMLprojectTitle.replace("%data%", projects.project[i].title);
+        var projectTitleString = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
         $(".project-entry:last").append(projectTitleString);
-        var projectDatesString = HTMLprojectDates.replace("%data%", projects.project[i].datesWorked);
+        var projectDatesString = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
         $(".project-entry:last").append(projectDatesString);
-        var projectDescriptionString = HTMLprojectDescription.replace("%data%", projects.project[i].description);
+        var projectDescriptionString = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
         $(".project-entry:last").append(projectDescriptionString);
-        for (var j = 0; j < projects.project[i].images.length; j++) {
+        for (var j = 0; j < projects.projects[i].images.length; j++) {
 
-            var projectImageString = HTMLprojectImage.replace("%data%", projects.project[i].images[j]);
+            var projectImageString = HTMLprojectImage.replace("%data%", projects.projects[i].images[j]);
             $(".project-entry:last").append(projectImageString);
 
         }
@@ -177,7 +179,7 @@ education.display = function() {
         $("#education").append(HTMLschoolStart);
         $(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[i].name));
         $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", education.schools[i].degree));
-        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[i].datesAttended));
+        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[i].dates));
         $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[i].location));
 
         for (var j = 0; j < education.schools[i].majors.length; j++) {
